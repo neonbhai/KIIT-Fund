3a2b406b3459ee923cddf911c574c0b506c633a3
Someshwar Tripathi <someshwartripathi8@gmail.com>
2023-04-29 01:01:10 +0530

    Initial Commit

---
+++ client/src/constants/index.js
@@ -0,0 +1,34 @@
+import { createCampaign, dashboard, logout, payment, profile, withdraw } from '../assets';
+
+export const navlinks = [
+  {
+    name: 'dashboard',
+    imgUrl: dashboard,
+    link: '/',
+  },
+  {
+    name: 'campaign',
+    imgUrl: createCampaign,
+    link: '/create-campaign',
+  },
+  {
+    name: 'payment',
+    imgUrl: payment,
+    link: '/',
+  },
+  {
+    name: 'withdraw',
+    imgUrl: withdraw,
+    link: '/',
+  },
+  {
+    name: 'profile',
+    imgUrl: profile,
+    link: '/profile',
+  },
+  {
+    name: 'logout',
+    imgUrl: logout,
+    link: '/',
+  },
+];
